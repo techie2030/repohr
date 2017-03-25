5fbc59f72a6adec027d12ad15f6846e5bde8ddf3
techie2030 <msreeni@outlook.com>
2017-03-24 20:55:58 -0400

    Initial Project set-up

---
+++ src/js/app.js
@@ -0,0 +1,12 @@
+/* Core application file */
+/*
+* Added new method test()
+*/
+var appName = "hello world";
+function init(){
+	console.log('App Init ::');
+}
+
+function test(){
+	console.log('In test method');
+}
